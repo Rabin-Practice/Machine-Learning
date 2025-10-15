6622bfccffc81c59dbeb32af3f393541339aa107
Rabin-24 <rabinamirtharaj333@gmail.com>
2025-10-15 16:01:32 +0530

    first commit

--- multiplediseaseprediction.py
+++ multiplediseaseprediction.py
@@ -2,18 +2,12 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-
 # loading the saved models
-
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
-
 heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
-
 parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
-
-
-
 # sidebar for navigation
+
 with st.sidebar:
     
     selected = option_menu('Multiple Disease Prediction System',
